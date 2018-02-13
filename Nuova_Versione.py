b79db026e8fd9c3aeb38de7526607eb3330ff3c9
davidemedusaureli <31849300+davidemedusaureli@users.noreply.github.com>
2018-02-13 20:51:06 +0100

    Modifiche

--- Nuova_Versione.py
+++ Nuova_Versione.py
@@ -9,34 +9,40 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 from pymongo import MongoClient
-import time
+import json
 
-
+#Davide
 #consumer key, consumer secret, access token, access secret.
 ckey="RquN0zhitB82NARp79NC9xXHb"
 csecret="77TJqc5dfBkdLso3uhx7UV5zHGPdbPUdxnlpn8KHCcINFGJAY8"
 atoken="931887582190895104-OXDeNHFHHwNTkQLIojdDvzvg7jGXgAg"
 asecret="04why4jJLITZMAMjTS0YIsdp85dO37Kgsx7ekhp5YsElZ"
 
+#Valerio
+#consumer key, consumer secret, access token, access secret.
+#ckey = "5n8QsFqTsfiaB9aqJJvGm01rn"
+#csecret = "QoKiYt9fAQkgRSeBMmFOokxBLYpmxJm4VDVLKMooWlPBnE5Jsp"
+#atoken =  "938705179284819968-3Sv1npwRRqbH2gcFiyAqwkH3gedDJqK"
+#asecret="62oNP7DxrDjWW1Dhb1Ud6HYEFaDrnJnQ4mt4vJKXI9AnA"
 
-connection = MongoClient('localhost', 27017)
-#connection.drop_database('TwitterProva') #drop database if already exists
-db = connection["TwitterProva"]
-#creation or re-creation of the database called HW3ADM
+
+client = MongoClient('localhost', 27017)
+client.drop_database('TwitterProva') #drop database if already exists
+db = client['Twitter_Prova']
+collection = db['Twitter_Renzi']
 
 class listener(StreamListener):
 
     def on_data(self, data):
-        try:
-            print(data)
-            saveFile = open("tweetDB.csv","a")
-            saveFile.write(data)
-            saveFile.write("\n")
-            saveFile.close()
-            return(True)
-        except BaseException:
-            print("failed on data")
-            time.sleep(5)
+        print(data)
+        data = data.replace("'", "")
+        json_acceptable_string = data.replace("'", "\"")
+        d = json.loads(json_acceptable_string)
+        posts = db.Twitter_Renzi
+        posts.insert_one(d)
+        print("ok")
+
+        return(True)
 
     def on_error(self, status):
         print (status)
@@ -47,3 +53,4 @@ auth.set_access_token(atoken, asecret)
 twitterStream = Stream(auth, listener())
 
 twitterStream.filter(follow=["13294452"])
+#13294452 @pdnetwork
