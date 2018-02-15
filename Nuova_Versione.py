860db59a6aadd95bba932f4abb3e2383a2c9514e
davidemedusaureli <31849300+davidemedusaureli@users.noreply.github.com>
2018-02-15 01:35:35 +0100

    Tweet

--- Nuova_Versione.py
+++ Nuova_Versione.py
@@ -13,34 +13,38 @@ import json
 
 #Davide
 #consumer key, consumer secret, access token, access secret.
-ckey="RquN0zhitB82NARp79NC9xXHb"
-csecret="77TJqc5dfBkdLso3uhx7UV5zHGPdbPUdxnlpn8KHCcINFGJAY8"
-atoken="931887582190895104-OXDeNHFHHwNTkQLIojdDvzvg7jGXgAg"
-asecret="04why4jJLITZMAMjTS0YIsdp85dO37Kgsx7ekhp5YsElZ"
+#ckey="RquN0zhitB82NARp79NC9xXHb"
+#csecret="77TJqc5dfBkdLso3uhx7UV5zHGPdbPUdxnlpn8KHCcINFGJAY8"
+#atoken="931887582190895104-OXDeNHFHHwNTkQLIojdDvzvg7jGXgAg"
+#asecret="04why4jJLITZMAMjTS0YIsdp85dO37Kgsx7ekhp5YsElZ"
 
 #Valerio
 #consumer key, consumer secret, access token, access secret.
-#ckey = "5n8QsFqTsfiaB9aqJJvGm01rn"
-#csecret = "QoKiYt9fAQkgRSeBMmFOokxBLYpmxJm4VDVLKMooWlPBnE5Jsp"
-#atoken =  "938705179284819968-3Sv1npwRRqbH2gcFiyAqwkH3gedDJqK"
-#asecret="62oNP7DxrDjWW1Dhb1Ud6HYEFaDrnJnQ4mt4vJKXI9AnA"
+ckey = "5n8QsFqTsfiaB9aqJJvGm01rn"
+csecret = "QoKiYt9fAQkgRSeBMmFOokxBLYpmxJm4VDVLKMooWlPBnE5Jsp"
+atoken =  "938705179284819968-3Sv1npwRRqbH2gcFiyAqwkH3gedDJqK"
+asecret="62oNP7DxrDjWW1Dhb1Ud6HYEFaDrnJnQ4mt4vJKXI9AnA"
 
 
 client = MongoClient('localhost', 27017)
-client.drop_database('TwitterProva') #drop database if already exists
+#client.drop_database('Twitter_Prova') #drop database if already exists
 db = client['Twitter_Prova']
-collection = db['Twitter_Renzi']
+collection = db['Twitter_Davide']
 
 class listener(StreamListener):
 
     def on_data(self, data):
         print(data)
+        
         data = data.replace("'", "")
         json_acceptable_string = data.replace("'", "\"")
         d = json.loads(json_acceptable_string)
-        posts = db.Twitter_Renzi
-        posts.insert_one(d)
-        print("ok")
+        userid = d["user"]["id"]
+        #print(type(str(d["is_quote_status"])))
+        if userid == 18762875 and "retweeted_status" not in d:
+            posts = db.Twitter_Renzi
+            posts.insert_one(d)
+            print("ok")
 
         return(True)
 
@@ -52,5 +56,8 @@ auth.set_access_token(atoken, asecret)
 
 twitterStream = Stream(auth, listener())
 
-twitterStream.filter(follow=["13294452"])
+twitterStream.filter(follow=["18762875"])
 #13294452 @pdnetwork
+#931887582190895104 @Davide
+#938705179284819968 @Valerio
+#Matteo Renzi 18762875
